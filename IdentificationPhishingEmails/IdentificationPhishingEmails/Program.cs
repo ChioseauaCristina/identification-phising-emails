c2f3fc20c2f517de5472c4eb436f1d12a3b8c1fd
robert.cioltea <robert.cioltea@theaccessgroup.com>
2023-11-12 14:58:11 +0200

    modify program.cs

--- IdentificationPhishingEmails/IdentificationPhishingEmails/Program.cs
+++ IdentificationPhishingEmails/IdentificationPhishingEmails/Program.cs
@@ -1,3 +1,5 @@
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+    });
+
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,11 +25,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.MapControllers();
 
