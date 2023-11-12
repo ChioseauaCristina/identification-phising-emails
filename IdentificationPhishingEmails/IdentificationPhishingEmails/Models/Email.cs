7533c169c4b8b6091944479c735d7ad34bda4b21
robert.cioltea <robertcioltea@gmail.com>
2023-11-12 17:15:43 +0200

    add first migration + model

---
+++ IdentificationPhishingEmails/IdentificationPhishingEmails/Models/Email.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentificationPhishingEmails.Models
+{
+    public class Email
+    {
+        public int Id { get; set; }
+        public string Receiver { get; set; }
+        public  string Sender { get; set ; }
+        public string Title { get; set; }
+        public  string Content { get; set; }
+    }
+}
